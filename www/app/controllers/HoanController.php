01bebc6dbe776a7469fee84cb233266e7af9c6e2
hohoan <hohoan87@gmail.com>
2014-07-18 23:08:36 +0700

    test

---
+++ www/app/controllers/HoanController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ho
+ * Date: 07/18/2014
+ * Time: 10:56 PM
+ */
+class HoanController extends BaseController
+{
+    public function getIndex()
+    {
+        echo 'hello word';
+    }
+}
